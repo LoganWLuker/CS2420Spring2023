bb5b11e2069e8badfb60a2d0fcfdcc2115b81681
Logan Luker <loganwluker@gmail.com>
2023-01-24 14:13:00 -0700

    Assignment02

---
+++ src/assign02/CS2420Student.java
@@ -0,0 +1,71 @@
+package assign02;
+import java.util.ArrayList;
+public class CS2420Student extends UofUStudent
+{
+	private EmailAddress email;
+	private ArrayList<Double> assignmentScores = new ArrayList<Double>();
+	private ArrayList<Double> examScores = new ArrayList<Double>();
+	private ArrayList<Double> labScores = new ArrayList<Double>();
+	private ArrayList<Double> quizScores = new ArrayList<Double>();
+	public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo)
+	{
+		super(firstName,lastName,uNID);
+		this.email = contactInfo;
+	}
+	public EmailAddress getContactInfo()
+	{
+		return this.email;
+	}
+	public void addScore(double score, String category)
+	{
+		switch (category) {
+			case "assignment": this.assignmentScores.add(score);
+			break;
+			case "exam" : this.examScores.add(score);
+			break;
+			case "lab" : this.labScores.add(score);
+			break;
+			case "quiz" : this.quizScores.add(score);
+			break;
+			default: break;
+		}
+	}
+	public double computeFinalScore() 
+	{
+		if(assignmentScores.size() == 0 || examScores.size() == 0 || labScores.size() == 0 || quizScores.size() == 0)
+			return 0.0;
+		double assignAv = 0;
+		double examAv = 0;
+		double labAv = 0;
+		double quizAv = 0;
+		double totalScore = 0;
+		for(int i = 0; i < assignmentScores.size(); i++)
+			assignAv += assignmentScores.get(i);
+		assignAv = assignAv/assignmentScores.size();
+		
+		for(int i = 0; i < examScores.size(); i++)
+			examAv += examScores.get(i);
+		examAv = examAv/examScores.size();
+		if(examAv < 65)
+			return examAv;
+		
+		for(int i = 0; i < labScores.size(); i++)
+			labAv += labScores.get(i);
+		labAv = labAv/labScores.size();
+		
+		for(int i = 0; i < quizScores.size(); i++)
+			quizAv += quizScores.get(i);
+		quizAv = quizAv/quizScores.size();
+		
+		totalScore = (0.40*assignAv) + (0.40*examAv) + (0.10*labAv) + (0.10*quizAv);
+		return totalScore;
+	}
+	public String computeFinalGrade()
+	{
+		if(assignmentScores.size() == 0 || examScores.size() == 0 || labScores.size() == 0 || quizScores.size() == 0)
+			return "N/A";
+		double totalScore = this.computeFinalScore();
+		
+		return "";
+	}
+}
