f5fc29055a07b0e6cbce37f7dbad47c2823995e2
Bruce-Crockett <brucrocrockett@gmail.com>
2023-02-22 19:01:48 -0700

    finalized sorter class and added tests

--- src/assign05/ArrayListSorter.java
+++ src/assign05/ArrayListSorter.java
@@ -1,10 +1,10 @@
 package assign05;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.NoSuchElementException;
+import java.util.Collections;
+
 /**
- * This class, given a generic array, can perform a mergesort or a quicksort, and can generate sorted ArrayLists given a size.
+ * This class, given a generic array, can perform a mergesort or a quicksort, and can generate sorted or permuted ArrayLists given a size.
  * @author Bruce Crockett and Logan Luker
  * @version February 22nd, 2023
  */
@@ -35,7 +35,7 @@ public class ArrayListSorter<T>
 	 * @param end	end of sorting range
 	 * @param temp	temporary array for storage
 	 */
-	public static <T extends Comparable<? super T>> void mergesortRec(ArrayList<T> arr, int beg, int end, ArrayList<T> temp)
+	private static <T extends Comparable<? super T>> void mergesortRec(ArrayList<T> arr, int beg, int end, ArrayList<T> temp)
 	{
 		//check if we should run insertion Sort
 		if(end - beg < threshold)
@@ -58,7 +58,7 @@ public class ArrayListSorter<T>
 	 * @param <T>	Type of ArrayList
 	 * @param arr	Array to be Sorted
 	 */
-	public static <T extends Comparable<? super T>> void insertionSort(ArrayList<T> arr)
+	private static <T extends Comparable<? super T>> void insertionSort(ArrayList<T> arr)
 	{
 		for(int i = 0; i < arr.size(); i++)
 		{
@@ -71,7 +71,16 @@ public class ArrayListSorter<T>
 		}
 	}
 	
-	public static <T extends Comparable<? super T>> void merge(ArrayList<T> arr, int beg, int mid, int end, ArrayList<T> temp)
+	/**
+	 * Merge routine, merges the two subarrays given by [beg, mid) and [mid, end)
+	 * @param <T>  type of array
+	 * @param arr  sup array containing subarrays to be merged
+	 * @param beg  first element of subarray to be merged
+	 * @param mid  last element of first subarray to be merged
+	 * @param end  last element of second subarray to be merged
+	 * @param temp storage for operations, copied to 'arr' when routine completes
+	 */
+	private static <T extends Comparable<? super T>> void merge(ArrayList<T> arr, int beg, int mid, int end, ArrayList<T> temp)
 	{
 		//Set up pointers
 		int i = beg, j = mid, k = beg;
@@ -93,21 +102,48 @@ public class ArrayListSorter<T>
 			arr.set(copy, temp.get(copy));
 	}
 	
+	/**
+	 * Quick Sort driver method and setup
+	 * @param <T>    type of array
+	 * @param arr    array to be sorted
+	 */
 	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> arr)
 	{
+		//check if empty
+		if(arr.size() <= 0)
+			throw new IllegalArgumentException("Array to be sorted cannot be empty");
+		
 		quicksortRec(arr,0,arr.size()-1);
 	}
-	public static <T extends Comparable<? super T>> void quicksortRec(ArrayList<T> arr, int begin, int end)
+	
+	/**
+	 * Recursive quicksort routine, driven by quicksort()
+	 * @param <T>     type of array
+	 * @param arr     array to be sorted
+	 * @param begin   first index of subarray to be sorted
+	 * @param end     last index of subarray to be sorted
+	 */
+	private static <T extends Comparable<? super T>> void quicksortRec(ArrayList<T> arr, int begin, int end)
 	{
 		if(begin < end)
 		{
-			//int partitionIndex = pivotA(arr, begin, end);
+			int partitionIndex = pivotA(arr, begin, end);
 			//int partitionIndex = pivotB(arr, begin, end);
-			int partitionIndex = pivotC(arr, begin, end);
+			//int partitionIndex = pivotC(arr, begin, end);
 			quicksortRec(arr, begin, partitionIndex-1);
 			quicksortRec(arr, partitionIndex+1, end);			
 		}
 	}
+	
+	/**
+	 * Partitions the given array by the last element in the array. Assumes
+	 * 	that the last element in the array is the pivot
+	 * @param <T>    type of ArrayList
+	 * @param arr    array to be partitioned
+	 * @param begin  first element of subarray to be partitioned
+	 * @param end    last element of subarray to be partitioned
+	 * @return
+	 */
 	private static <T extends Comparable<? super T>> int partition(ArrayList<T> arr, int begin, int end) 
 	{
 	    T pivot = arr.get(end);
@@ -129,6 +165,14 @@ public class ArrayListSorter<T>
 
 	    return i+1;
 	}
+	/**
+	 * Generates a pivot for partition by picking a random index
+	 * @param <T>   type of ArrayList
+	 * @param arr   array to be partitioned
+	 * @param begin first index of subarray to be partitioned
+	 * @param end   last index of subarray to be partitioned
+	 * @return
+	 */
 	private static <T extends Comparable<? super T>> int pivotA(ArrayList<T> arr, int begin, int end)
 	{
 		int r = (int)(Math.random()*(end-begin))+begin;	//pivot at a random point in the range
@@ -137,6 +181,15 @@ public class ArrayListSorter<T>
 	    arr.set(end, swapTemp);
 		return partition(arr,begin,end);
 	}
+	
+	/**
+	 * Generates a pivot for partition by picking the center index
+	 * @param <T>   type of ArrayList
+	 * @param arr   array to be partitioned
+	 * @param begin first index of subarray to be partitioned
+	 * @param end   last index of subarray to be partitioned
+	 * @return
+	 */
 	private static <T extends Comparable<? super T>> int pivotB(ArrayList<T> arr, int begin, int end)
 	{
 		int r = (begin+end)/2;				//pivot the middle of the range
@@ -145,6 +198,15 @@ public class ArrayListSorter<T>
 	    arr.set(end, swapTemp);
 		return partition(arr,begin,end);	
 	}
+	
+	/**
+	 * Generates a pivot for partition by picking the first index of the ArrayList
+	 * @param <T>   type of ArrayList
+	 * @param arr   array to be partitioned
+	 * @param begin first index of subarray to be partitioned
+	 * @param end   last index of subarray to be partitioned
+	 * @return
+	 */
 	private static <T extends Comparable<? super T>> int pivotC(ArrayList<T> arr, int begin, int end)
 	{
 		int r = begin;	//pivot at the first point in the range
@@ -154,18 +216,61 @@ public class ArrayListSorter<T>
 		return partition(arr,begin,end);
 	}
 	
+	/**
+	 * Generates an ArrayList of integers from 1...size in ascending order
+	 * @param size   size of ArrayList to be generated
+	 * @return       generated ArrayList
+	 */
 	public static ArrayList<Integer> generateAscending(int size)
 	{
-		return null;
+		if(size<=0)
+			throw new IllegalArgumentException("Array to be generated cannot be of size 0 or below");
+		
+		ArrayList<Integer> toReturn = new ArrayList<Integer>(size);
+		for(int i = 1; i <= size; i++)
+		{
+			toReturn.add(i);
+		}
+		
+		return toReturn;
 	}
 	
+	/**
+	 * Generates an ArrayList of integers from 1...size in ascending order,
+	 * 	then permutes the ArrayList using java.Collections.shuffle().
+	 * @param size   size of ArrayList to be generated
+	 * @return       generated ArrayList
+	 */
 	public static ArrayList<Integer> generatePermuted(int size)
 	{
-		return null;
+		if(size<=0)
+			throw new IllegalArgumentException("Array to be generated cannot be of size 0 or below");
+		
+		ArrayList<Integer> toReturn = new ArrayList<Integer>(size);
+		for(int i = 1; i <= size; i++)
+		{
+			toReturn.add(i);
+		}
+		Collections.shuffle(toReturn);
+		return toReturn;
 	}
 	
+	/**
+	 * Generates an ArrayList of integers from 1...size in descending order
+	 * @param size   size of ArrayList to be generated
+	 * @return       generated ArrayList
+	 */
 	public static ArrayList<Integer> generateDescending(int size)
 	{
-		return null;
+		if(size<=0)
+			throw new IllegalArgumentException("Array to be generated cannot be of size 0 or below");
+		
+		ArrayList<Integer> toReturn = new ArrayList<Integer>(size);
+		for(int i = size; i >= 1; i--)
+		{
+			toReturn.add(i);
+		}
+		
+		return toReturn;
 	}
 }
--- src/assign05/ArrayListSorterTester.java
+++ src/assign05/ArrayListSorterTester.java
@@ -8,59 +8,306 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class provides extensive tests for the public methods in the ArrayListSorter class.
+ * @author Bruce Crockett and Logan Luker
+ * @version February 22nd, 2023
+ */
 public class ArrayListSorterTester 
 {
-	//if testing, make merge() public instead of private
+	// Empty tests -------------------
+	
 	@Test
-	void mergeDevTest()
+	void emptyArrayMergeTest()
 	{
-		ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(4,8,11,25,2,3,17,20,30));
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		for (int i = 0; i < arr.size(); i++)
-			temp.add(i,-1);
-		
-		ArrayListSorter.merge(arr, 0,4,9, temp);
-		arr = temp;
-		assertEquals(new ArrayList<Integer>(Arrays.asList(2,3,4,8,11,17,20,25,30)), arr);
+		ArrayList<Integer> empty = new ArrayList<Integer>();
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.mergesort(empty));
 	}
 	
-	
-//	@Test
-//	void mergesortDevTest()
-//	{
-////		ArrayList<Integer> arr1 = new ArrayList<Integer>();
-////		arr1.add(4);
-////		arr1.add(2);
-////		arr1.add(5);
-////		arr1.add(1);
-////		arr1.add(3);
-////		arr1.add(0);
-//		ArrayList<Integer> arr1 = new ArrayList<Integer>(Arrays.asList(510510,5131,6848,565,15,210515,451,4,8));
-//		ArrayList<Integer> out = new ArrayList<Integer>();
-//		
-//		for(int i = 0; i < 6; i++)
-//		{
-//			out.add(-1);
-//		}
-//		
-//		
-//		ArrayListSorter.mergesort(arr1);
-//		
-//		for(Integer e : arr1)
-//		{
-//			System.out.print(e + ",");
-//		}
-//		
-//	}
 	@Test
-	void quicksortTest()
+	void emptyArrayQuickTest()
 	{
-		ArrayList<Integer> arr2 = new ArrayList<Integer>(Arrays.asList(510510,5131,6848,565,15,210515,451,4,8));
+		ArrayList<Integer> empty = new ArrayList<Integer>();
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.quicksort(empty));
+	}
+	
+	@Test
+	void undersizedAscTest()
+	{
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generateAscending(0));
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generateAscending(-1));
+	}
+	
+	@Test
+	void undersizedPermTest()
+	{
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generatePermuted(0));
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generatePermuted(-1));
+	}
+	
+	@Test
+	void undersizedDescTest()
+	{
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generateDescending(0));
+		assertThrows(IllegalArgumentException.class,() -> ArrayListSorter.generateDescending(-1));
+	}
+	
+	// mergesort tests ---------------
+	
+	@Test
+	void mergeOneTest()
+	{
+		ArrayList<Integer> zero = new ArrayList<Integer>();
+		ArrayList<Integer> one = new ArrayList<Integer>();
+		ArrayList<Integer> minusOne = new ArrayList<Integer>();
+		zero.add(0);
+		one.add(1);
+		minusOne.add(-1);
+		
+		ArrayListSorter.mergesort(zero);
+		ArrayListSorter.mergesort(one);
+		ArrayListSorter.mergesort(minusOne);
+		
+		assertEquals(one.get(0), 1);
+		assertEquals(zero.get(0), 0);
+		assertEquals(minusOne.get(0), -1);
+	}
+	
+	@Test
+	void mergeTwoTest()
+	{
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		ArrayList<Integer> arr2 = new ArrayList<Integer>();
+		ArrayList<Integer> arr3 = new ArrayList<Integer>();
+		ArrayList<Integer> arr4 = new ArrayList<Integer>();
+		
+		arr1.add(0);
+		arr1.add(1);
+		
+		arr2.add(1);
+		arr2.add(0);
+		
+		arr3.add(1);
+		arr3.add(-1);
+		
+		arr4.add(-10);
+		arr4.add(-11);
+		
+		ArrayListSorter.mergesort(arr1);
+		ArrayListSorter.mergesort(arr2);
+		ArrayListSorter.mergesort(arr3);
+		ArrayListSorter.mergesort(arr4);
+		
+		assertEquals(arr1.get(0), 0);
+		assertEquals(arr1.get(1), 1);
+		
+		assertEquals(arr2.get(0), 0);
+		assertEquals(arr2.get(1), 1);
+		
+		assertEquals(arr3.get(0), -1);
+		assertEquals(arr3.get(1), 1);
+		
+		assertEquals(arr4.get(0), -11);
+		assertEquals(arr4.get(1), -10);
+	}
+	
+	@Test
+	void mergeTest()
+	{
+		ArrayList<Integer> arr1 = new ArrayList<Integer>(Arrays.asList(50, 0, 1, 3, 2, 49, 48));
+		ArrayList<Integer> arr2 = new ArrayList<Integer>(Arrays.asList(3, 2, 1, 0, -1, -2, -3));
+		ArrayList<Integer> arr3 = new ArrayList<Integer>(Arrays.asList(1, 0, 0, 0, 0));
+		
+		ArrayListSorter.mergesort(arr1);
+		ArrayListSorter.mergesort(arr2);
+		ArrayListSorter.mergesort(arr3);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0,1,2,3,48,49,50)), arr1);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(-3,-2,-1,0,1,2,3)), arr2);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0,0,0,0,1)), arr3);
+	}
+	
+	@Test
+	void largeMergeTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateDescending(500);
+		ArrayList<Integer> arr2 = ArrayListSorter.generatePermuted(500);
+		ArrayList<Integer> arr3 = ArrayListSorter.generateAscending(500);
+		
+		ArrayListSorter.mergesort(arr1);
+		ArrayListSorter.mergesort(arr2);
+		ArrayListSorter.mergesort(arr3);
+		
+		assertEquals(ArrayListSorter.generateAscending(500), arr1);
+		assertEquals(ArrayListSorter.generateAscending(500), arr2);
+		assertEquals(ArrayListSorter.generateAscending(500), arr3);
+	}
+	
+	// quicksort tests ---------------
+	
+	@Test
+	void quickOneTest()
+	{
+		ArrayList<Integer> zero = new ArrayList<Integer>();
+		ArrayList<Integer> one = new ArrayList<Integer>();
+		ArrayList<Integer> minusOne = new ArrayList<Integer>();
+		zero.add(0);
+		one.add(1);
+		minusOne.add(-1);
+		
+		ArrayListSorter.quicksort(zero);
+		ArrayListSorter.quicksort(one);
+		ArrayListSorter.quicksort(minusOne);
+		
+		assertEquals(one.get(0), 1);
+		assertEquals(zero.get(0), 0);
+		assertEquals(minusOne.get(0), -1);
+	}
+	
+	@Test
+	void quickTwoTest()
+	{
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		ArrayList<Integer> arr2 = new ArrayList<Integer>();
+		ArrayList<Integer> arr3 = new ArrayList<Integer>();
+		ArrayList<Integer> arr4 = new ArrayList<Integer>();
+		
+		arr1.add(0);
+		arr1.add(1);
+		
+		arr2.add(1);
+		arr2.add(0);
+		
+		arr3.add(1);
+		arr3.add(-1);
+		
+		arr4.add(-10);
+		arr4.add(-11);
+		
+		ArrayListSorter.quicksort(arr1);
 		ArrayListSorter.quicksort(arr2);
-		//System.out.println("\t");
-		for(Integer e : arr2)
+		ArrayListSorter.quicksort(arr3);
+		ArrayListSorter.quicksort(arr4);
+		
+		assertEquals(arr1.get(0), 0);
+		assertEquals(arr1.get(1), 1);
+		
+		assertEquals(arr2.get(0), 0);
+		assertEquals(arr2.get(1), 1);
+		
+		assertEquals(arr3.get(0), -1);
+		assertEquals(arr3.get(1), 1);
+		
+		assertEquals(arr4.get(0), -11);
+		assertEquals(arr4.get(1), -10);
+	}
+	
+	@Test
+	void quickTest()
+	{
+		ArrayList<Integer> arr1 = new ArrayList<Integer>(Arrays.asList(50, 0, 1, 3, 2, 49, 48));
+		ArrayList<Integer> arr2 = new ArrayList<Integer>(Arrays.asList(3, 2, 1, 0, -1, -2, -3));
+		ArrayList<Integer> arr3 = new ArrayList<Integer>(Arrays.asList(1, 0, 0, 0, 0));
+		
+		ArrayListSorter.quicksort(arr1);
+		ArrayListSorter.quicksort(arr2);
+		ArrayListSorter.quicksort(arr3);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0,1,2,3,48,49,50)), arr1);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(-3,-2,-1,0,1,2,3)), arr2);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0,0,0,0,1)), arr3);
+	}
+	
+	@Test
+	void largeQuickTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateDescending(500);
+		ArrayList<Integer> arr2 = ArrayListSorter.generatePermuted(500);
+		ArrayList<Integer> arr3 = ArrayListSorter.generateAscending(500);
+		
+		ArrayListSorter.quicksort(arr1);
+		ArrayListSorter.quicksort(arr2);
+		ArrayListSorter.quicksort(arr3);
+		
+		assertEquals(ArrayListSorter.generateAscending(500), arr1);
+		assertEquals(ArrayListSorter.generateAscending(500), arr2);
+		assertEquals(ArrayListSorter.generateAscending(500), arr3);
+	}
+	
+	// ascending tests ---------------
+	
+	@Test
+	void ascendingSmallTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateAscending(1);
+		ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(2);
+		ArrayList<Integer> arr3 = ArrayListSorter.generateAscending(4);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1)), arr1);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2)), arr2);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,3,4)), arr3);
+	}
+	
+	@Test
+	void ascendingTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateAscending(10);
+		ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(1000);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10)), arr1);
+		
+		for(int i = 1; i <= 1000; i++)
 		{
-			System.out.print(e + ",");
+			assertEquals(i, arr2.get(i-1));
+		}
+	}
+	
+	// permuted tests ----------------
+	
+	@Test
+	void permOneTest()
+	{
+		ArrayList<Integer> one = ArrayListSorter.generatePermuted(1);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1)), one);
+	}
+	
+	@Test
+	void permBoundTest()
+	{
+		ArrayList<Integer> perm = ArrayListSorter.generatePermuted(500);
+		
+		for(Integer i : perm)
+		{
+			if(i.intValue() < 1 || i.intValue() > 500)
+				fail("Out of bounds");
+		}
+	}
+	// descending tests --------------
+
+	@Test
+	void descendingSmallTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateDescending(1);
+		ArrayList<Integer> arr2 = ArrayListSorter.generateDescending(2);
+		ArrayList<Integer> arr3 = ArrayListSorter.generateDescending(4);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1)), arr1);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(2,1)), arr2);
+		assertEquals(new ArrayList<Integer>(Arrays.asList(4,3,2,1)), arr3);
+	}
+	
+	@Test
+	void descendingTest()
+	{
+		ArrayList<Integer> arr1 = ArrayListSorter.generateDescending(10);
+		ArrayList<Integer> arr2 = ArrayListSorter.generateDescending(1000);
+		
+		assertEquals(new ArrayList<Integer>(Arrays.asList(10,9,8,7,6,5,4,3,2,1)), arr1);
+		
+		for(int i = 1000; i >= 1; i--)
+		{
+			assertEquals(i, arr2.get(1000-i));
 		}
 	}
 }
