04e244702eabb815837787634d4ba524606fafd2
Bruce-Crockett <brucrocrockett@gmail.com>
2023-03-02 21:22:47 -0700

    more tests for iterator, hopefully didn't break anything with weird binary stuff

--- src/assign06/SinglyLinkedListTest.java
+++ src/assign06/SinglyLinkedListTest.java
@@ -87,4 +87,118 @@ public class SinglyLinkedListTest
 		Object[] test = {1, 2, 3, 4};
 		assertArrayEquals(arr, test);
 	}
+	
+	@Test
+	public void iteratorTest()
+	{
+		SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+		list.insertFirst(4);
+		list.insertFirst(3);
+		list.insertFirst(2);
+		list.insertFirst(1);
+		SinglyLinkedList<Integer>.SLLIterator iter = (SinglyLinkedList<Integer>.SLLIterator) list.iterator();
+		
+		assertTrue(iter.hasNext());
+		assertEquals(2, iter.next());
+		
+		assertTrue(iter.hasNext());
+		assertEquals(3, iter.next());
+		
+		assertTrue(iter.hasNext());
+		assertEquals(4, iter.next());
+		
+		assertFalse(iter.hasNext());
+		
+		iter.remove();
+		
+		assertEquals(list.get(0), 1);
+		assertEquals(list.get(1), 2);
+		assertEquals(list.get(2), 3);
+		assertThrows(IndexOutOfBoundsException.class, () -> list.get(3));
+		assertEquals(list.size(), 3);
+		
+		SinglyLinkedList<Integer> list2 = new SinglyLinkedList<Integer>();
+		list2.insertFirst(4);
+		list2.insertFirst(3);
+		list2.insertFirst(2);
+		list2.insertFirst(1);
+		SinglyLinkedList<Integer>.SLLIterator iter2 = (SinglyLinkedList<Integer>.SLLIterator) list2.iterator();
+		
+		assertTrue(iter2.hasNext());
+		assertEquals(2, iter2.next());
+		
+		iter2.remove();
+		
+		assertTrue(iter2.hasNext());
+		assertEquals(3, iter2.next());
+		
+		assertTrue(iter2.hasNext());
+		assertEquals(4, iter2.next());
+		
+		assertFalse(iter2.hasNext());
+		
+		assertEquals(list2.get(0), 1);
+		assertEquals(list2.get(1), 3);
+		assertEquals(list2.get(2), 4);
+		assertThrows(IndexOutOfBoundsException.class, () -> list2.get(3));
+		
+		SinglyLinkedList<Integer> list3 = new SinglyLinkedList<Integer>();
+		list3.insertFirst(4);
+		list3.insertFirst(3);
+		list3.insertFirst(2);
+		list3.insertFirst(1);
+		SinglyLinkedList<Integer>.SLLIterator iter3 = (SinglyLinkedList<Integer>.SLLIterator) list3.iterator();
+		
+		assertTrue(iter3.hasNext());
+		assertEquals(2, iter3.next());
+		
+		assertTrue(iter3.hasNext());
+		assertEquals(3, iter3.next());
+		
+		iter3.remove();
+		
+		assertTrue(iter3.hasNext());
+		assertEquals(4, iter3.next());
+		
+		assertFalse(iter3.hasNext());
+		
+		assertEquals(list3.get(0), 1);
+		assertEquals(list3.get(1), 2);
+		assertEquals(list3.get(2), 4);
+		assertThrows(IndexOutOfBoundsException.class, () -> list3.get(3));
+	}
+	
+	@Test
+	public void iteratorTestTwo()
+	{
+		SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+		list.insertFirst(4);
+		list.insertFirst(3);
+		list.insertFirst(2);
+		list.insertFirst(1);
+		SinglyLinkedList<Integer>.SLLIterator iter = (SinglyLinkedList<Integer>.SLLIterator) list.iterator();
+		
+		assertThrows(IllegalStateException.class, () -> iter.remove());
+		
+		iter.next();
+		iter.remove();
+		iter.next();
+		iter.remove();
+		iter.next();
+		iter.remove();
+		
+		assertEquals(1, list.get(0));
+		assertEquals(1, list.size());
+		
+		SinglyLinkedList<Integer> list2 = new SinglyLinkedList<Integer>();
+		list2.insertFirst(4);
+		list2.insertFirst(3);
+		list2.insertFirst(2);
+		list2.insertFirst(1);
+		SinglyLinkedList<Integer>.SLLIterator iter2 = (SinglyLinkedList<Integer>.SLLIterator) list2.iterator();
+		
+		assertEquals(2, iter2.next());
+		assertEquals(3, iter2.next());
+		assertEquals(4, iter2.next());
+	}
 }
